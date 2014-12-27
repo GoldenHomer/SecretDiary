1861e675b92d964a0ebdf3c691509037f4296a3d
Jose Contreras <jocontre@joselcontreras.com>
2014-12-26 23:18:00 -0600

    I think I'm doing this wrong

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 	
+	session_start();
+
 	if ($_POST['submit']) {
 		if (!$_POST['email']) {
 			$error.="Please enter your email.";
@@ -13,11 +15,11 @@
 		}
 		else {
 			if (strlen($_POST['password']) < 8) {
-				$error.="Please enter a password."
+				$error.="Please enter a password.";
 			}
 
 			if (!preg_match('`[A-Z]`', $_POST['password'])) {
-				$error.="Include at least one capital letter in your password."
+				$error.="Include at least one capital letter in your password.";
 			}
 		}
 
@@ -25,7 +27,29 @@
 			echo "There were error(s) in your signup info: ".$error;
 		}
 		else {
-			
+			$link = mysqli_connect("localhost", "cl54-firstdb-2oe", "9DuSsSk_4MjNFEk", "cl54-firstdb-2oe");
+			$query="SELECT * FROM users WHERE email='".mysqli_real_escape_string($link, $_POST['email'])."'"; // Whoa, all these double and single quotes look nasty.
+
+			$result = mysqli_query($link, $query);
+
+			echo $results = mysqli_num_rows($result);
+
+			if ($results) {
+				echo "That email address is already registered. Perhaps you want to log in?";
+			}
+			else {
+				$query="INSERT INTO 'users' ('email', 'password') VALUES('".mysqli_real_escape_string($link, $_POST['email'])."', md5(md5(%_POST['email']).$_POST['password']))";
+
+				mysqli_query($link, $query);
+
+				echo "You have signed up.";
+
+				$_SESSION['id']=mysqli_insert_id($link);
+
+				print_r($_SESSION);
+
+				//Redirect to logged-in page.
+			}
 		}
 	}
 
