5204fba97c0c0830e8864aa0f9dc1aef3d4d920b
Jose Contreras <jocontre@joselcontreras.com>
2014-12-28 11:52:15 -0600

    Progress

--- index.php
+++ index.php
@@ -26,15 +26,10 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand">ThisApp</a>
+					<a class="navbar-brand">Diary</a>
 				</div>
 
 				<div class="collapse navbar-collapse">
-					<ul class="nav navbar-nav">
-						<li class="active"><a href="#background">Home</a></li>
-						<li><a href="#details">About</a></li>
-						<li><a href="#footer">Download the app</a></li>
-					</ul>
 					<form class="navbar-form navbar-right">
 						<div class="form-group">
 							<input type="email" placeholder="Email" class="form-control">
@@ -49,59 +44,24 @@
 		<div class="container contentContainer" id="background">
 			<div class="row">
 				<div class="col-md-6 col-md-offset-3" id="topRow">
-					<h1 class="topMargin">SV World-Changing App #41903457</h1>
-					<p class="lead">This will not make any money.</p>
-					<p>Product Hunt hosts many examples.</p>
-					<p class="bold topMargin">Interested in reality? Join the mailing list.</p>
+					<h1 class="topMargin">Diary of a Mad Woman</h1>
+					<p class="bold topMargin">Your own private diary. No, the NSA doesn't care enough.</p>
 
-					<form class="topMargin">
+					<form class="topMargin" method="post">
 						<div class="input-group">
-							<span class="input-group-addon">@</span>
+							<label form="email">Email Address</label>
 							<input type="email" class="form-control" placeholder="Your email" />
 						</div>
+
+						<div class="input-group">
+							<label form="password">Password</label>
+							<input type="password" class="form-control" placeholder="Password" />
+						</div>
 						
-						<input type="submit" class="btn btn-success btn-lg topMargin" />
+						<input type="submit" name="submit" value="Sign Up" class="btn btn-success btn-lg topMargin" />
 
 					</form>
 				</div>
-			</div>
-		</div>
-
-		<div class="container contentContainer" id="details">
-			<div class="row center">
-				<h1 class="center title">This app is a bigger failure than paying $3000+ rent to temporarily forget homeless people live outside your SOMA closet.</h1>
-				<p class="lead center">But what makes it sweet is you can order $4 toast and bagels flown in from NY.</p>
-			</div>
-
-			<div class="row marginBottom">
-				<div class="col-md-4 topMargin">
-					<h2><span class="glyphicon glyphicon-ban-circle"></span>Header</h2>
-					Can't forget what Austin is like now.
-					<button class="btn btn-success topMargin">Sign me up please.</button> 
-				</div>
-
-				<div class="col-md-4 topMargin">
- 	 	 			<h2><span class="glyphicon glyphicon-off"></span>Header</h2>
- 	 	 			<p>Live-music-capital-of-the-world title does not mean the music is good. What does it take to get a gig? An acoustic guitar and your band being named Fedora Cuff Chukka and the Magnesium Whistleblowers?</p>
- 	 	 			
- 	 	 			<button class="btn btn-success marginTop">Sign Up!</button>
- 	 	 	
- 	 	 		</div>
-
- 	 	 		<div class="col-md-4 marginTop">
- 	 	 			<h2><span class="glyphicon glyphicon-pencil"></span> Header</h2>
- 	 	 			<p>But Austin is slightly better than College Station. Junny Foosball.</p>
- 	 	 			
- 	 	 			<button class="btn btn-success marginTop">Sign Up!</button>
- 	 	 		</div>
-			</div>
-		</div>
-
-		<div class="container contentContainer" id="footer">
-			<div class="row">
-				<h1 class="center">Download the app</h1>
-				<p class="lead">Only for Windows smartphones.</p>
-				<p class="center"><img src="win_store.png" class="appStoreImg"></p>
 			</div>
 		</div>
 
